9bfda3556e96d6aaf839d288be9d6cd659646263
studio-11 <131365144+studio-11@users.noreply.github.com>
2024-12-04 10:19:45 +0000

    Deploying to gh-pages from @ studio-11/html-bootstrap-editor-showcase-staging@60350e6fd1291de77dbcba75daca2b5dd8511518 🚀

--- js/vitrine.js
+++ js/vitrine.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+    // Simuler un clic sur le filtre "Contenu" au chargement de la page
+    $('.badgefilter').each(function(){
+        if ($(this).text().toLowerCase() === 'contenu') {
+            $(this).addClass('selected'); // Ajouter la classe "selected" pour activer le filtre
+        }
+    });
+
+    // Mettre à jour l'affichage des éléments en fonction du filtre par défaut
+    searchTpl();
+
+    // Gestion des clics pour afficher les templates
     $('.tile').click(function(){
         var url = $(this).attr('file');
         var desc = $(this).attr('desc');
@@ -6,24 +17,28 @@ $(document).ready(function(){
         $('#modal-title').html($(this).attr('title'))
         $('#modal-body').html("<img class='modal-img' src='"+img+"'><p>"+desc+"<p/><div class=\"btn-group\"><a href='#' class='btn btn-success' onclick=\"importTemplate('"+url+"')\">Import</a></div>");
         $('#modal').modal('show');
-    })
+    });
+
     $('.close').click(function(){
         $('#modal').modal('hide');
-    })
+    });
 
-    $('#search').on('input',function(){
-        searchTpl()
-    })
-    
+    // Recherche par texte
+    $('#search').on('input', function(){
+        searchTpl();
+    });
+
+    // Gestion des filtres
     $('.badgefilter').click(function(){
         if($(this).hasClass('selected')){
-            $(this).removeClass('selected')
+            $(this).removeClass('selected');
         }else{
-            $(this).addClass('selected')
+            $(this).addClass('selected');
         }
-        searchTpl()
-    })
+        searchTpl();
+    });
 });
+
 
 function searchTpl(){
     var val = $('#search').val().toLowerCase();
